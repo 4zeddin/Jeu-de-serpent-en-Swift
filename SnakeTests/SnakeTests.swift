ffbcb5d8bf5c87a846490d8184e4f1c14c0f5515
4zeddin <azeddinaitwaalla@gmail.com>
2024-05-17 12:58:01 +0100

    h

---
+++ SnakeTests/SnakeTests.swift
@@ -0,0 +1,27 @@
+
+import XCTest
+@testable import Snake
+
+class SnakeTests: XCTestCase {
+
+    override func setUp() {
+        // 
+        }
+
+    override func tearDown() {
+        
+        //
+        }
+
+    func testExample() {
+        // 
+        }
+
+    func testPerformanceExample() {
+        
+        self.measure {
+            // 
+            }
+    }
+
+}
---
+++ SnakeUITests/SnakeUITests.swift
@@ -0,0 +1,24 @@
+import XCTest
+
+class SnakeUITests: XCTestCase {
+
+    override func setUp() {
+        
+        
+        continueAfterFailure = false
+
+        
+        XCUIApplication().launch()
+
+        
+         }
+
+    override func tearDown() {
+        // 
+        }
+
+    func testExample() {
+        // 
+        }
+
+}
